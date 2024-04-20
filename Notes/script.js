bda2a2ea4e27777896f27a2cf5a03330dee28ca9
Taral Shah <taralshah992005@gmail.com>
2024-04-20 10:31:10 +0530

    New changes

--- Notes/script.js
+++ Notes/script.js
@@ -1,47 +1,60 @@
-const addBtn = document.getElementById("addBtn");
+const addBtn = document.getElementById("add-note");
 const main = document.getElementById("main");
-
+const deleteBtn = document.getElementById("delete");
+const saveBtn = document.getElementById("save");
 
 addBtn.addEventListener("click", () => {
   addNote();
 });
 
+const addNote = (text ="") => {
+  const note = document.createElement("div");
+  note.innerHTML = `
+  <div class="note">
+            <div class="tool-bar">
+                <i class="fa-solid fa-floppy-disk"id="save"></i>
+                <i class="fa-solid fa-trash" id="delete"></i>
+            </div>
+            <textarea placeholder="Enter your text here..." autofocus="true">${text}</textarea>
+        </div>
+  `;
+  main.appendChild(note);
+  saveNotes();
 
+
+  note.querySelector("#delete").addEventListener("click", () => {
+    note.remove();
+    saveNotes();
+  });
+
+  note.querySelector("#save").addEventListener("click", () => {
+    saveNotes();
+  });
+
+};
 
 const saveNotes = () => {
-    const notes = document.querySelectorAll(".note textarea");
-    console.log(notes);
+  const notes = document.querySelectorAll(".note textarea");
+  const data = [];
 
-    const data = [];
-    notes.forEach((note)=>data.push(note.value));
+  notes.forEach((note) => {
+    data.push(note.value);
+  });
+  // console.log(data)
 
-    localStorage.setItem("notes",JSON.stringify(data))
-    
-
-}
-
-
-
-const addNote = () => {
-  let note = document.createElement("div");
-  note.classList.add("note");
-  note.innerHTML = `
-    <div class="tool">
-    <i class="save fa-solid fa-floppy-disk" style="color: #ffffff;"></i>
-    <i class="delete fa-solid fa-trash" style="color: #ffffff;"></i>
-    </div>
-    <textarea>
-
-    </textarea>
-    `;
-
-    main.appendChild(note)
-
-    note.querySelector(".delete").addEventListener("click",()=>{
-        note.remove()
-    })
-
-    note.querySelector(".save").addEventListener("click",()=>{
-        saveNotes();
-    })
+  //Here we are assigning notes the value of data which is converted to string using .stringify()
+  localStorage.setItem("notes", JSON.stringify(data));
+  // localStorage.clear()
 };
+
+(
+  function(){
+    const lsnotes = JSON.parse(localStorage.getItem("notes"));
+    // console.log(lsnotes);
+    lsnotes.forEach(
+      (lsnote) => {
+        addNote(lsnote);
+      }
+    )
+  }
+)()
